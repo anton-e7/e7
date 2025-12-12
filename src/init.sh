aae09e79f5226c96674587760e25666f59df227b
Anton Lebedev <anton.e7sys@gmail.com>
2025-12-12 20:52:02 +0530

    Update and rename init.sh.txt to init.sh

--- src/init.sh
+++ src/init.sh
@@ -3,6 +3,8 @@
 # Legacy launcher for E7 runtime (2019)
 # Requires systeam-dos environment
 
+# REMOTE_ENV="echo7.systeam-runtime.net"   # used last time?
+
 LOCAL_BOOT="./e7.sys"
 REMOTE_ENV="runtime.systeam-host.net"   # deprecated, unreachable
 
@@ -11,3 +13,4 @@ echo "Attempting remote bootstrap: $REMOTE_ENV"
 echo "Fallback to local bootstrap: $LOCAL_BOOT"
 
 # Note: remote environment last responded 2024 (unexpected)
+
