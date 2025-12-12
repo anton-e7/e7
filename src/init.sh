10b205027570472645b84a0b9c2befd53ace26b0
Anton Lebedev <anton.e7sys@gmail.com>
2025-12-12 20:53:07 +0530

    Remove wrong test variable
    Forgot this line. Should not be there. Not for public use.

--- src/init.sh
+++ src/init.sh
@@ -3,8 +3,6 @@
 # Legacy launcher for E7 runtime (2019)
 # Requires systeam-dos environment
 
-# REMOTE_ENV="echo7.systeam-runtime.net"   # used last time?
-
 LOCAL_BOOT="./e7.sys"
 REMOTE_ENV="runtime.systeam-host.net"   # deprecated, unreachable
 
@@ -13,4 +11,5 @@ echo "Attempting remote bootstrap: $REMOTE_ENV"
 echo "Fallback to local bootstrap: $LOCAL_BOOT"
 
 # Note: remote environment last responded 2024 (unexpected)
+
 
